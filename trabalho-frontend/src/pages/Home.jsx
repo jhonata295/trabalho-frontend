9c9c2e95e76d09ccb56c1b477f6aac8b1610ce2d
jhonata295 <jhonata.matos@iesb.edu.br>
2026-06-06 00:12:23 -0300

    feat: adicionar estatisticas ao dashboard

--- trabalho-frontend/src/pages/Home.jsx
+++ trabalho-frontend/src/pages/Home.jsx
@@ -4,15 +4,32 @@ import { TeamContext } from "../context/TeamContext";
 function Home() {
   const { times } = useContext(TeamContext);
 
+  const ultimosTimes = [...times].slice(-3);
+
   return (
     <div>
-      <h1>FutManager</h1>
+      <h1>⚽ FutManager</h1>
 
       <h2>Sistema de Gestão de Times de Futebol</h2>
 
-      <h3>Total de Times Cadastrados</h3>
+      <div>
+        <h3>Total de Times Cadastrados</h3>
+        <p>{times.length}</p>
+      </div>
 
-      <p>{times.length}</p>
+      <div>
+        <h3>Últimos Times Cadastrados</h3>
+
+        {ultimosTimes.length === 0 ? (
+          <p>Nenhum time cadastrado.</p>
+        ) : (
+          ultimosTimes.map((time) => (
+            <div key={time.id}>
+              <p>{time.nome}</p>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 }
