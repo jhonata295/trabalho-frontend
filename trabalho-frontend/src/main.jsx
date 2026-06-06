bf51078a0cc973a548f193e68fa580ae9cd3b7b3
jhonata295 <jhonata.matos@iesb.edu.br>
2026-06-06 00:13:43 -0300

    style: adicionar estilos globais

--- trabalho-frontend/src/main.jsx
+++ trabalho-frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import "./styles/global.css";
 import App from "./App";
 import { TeamProvider } from "./context/TeamContext";
 
