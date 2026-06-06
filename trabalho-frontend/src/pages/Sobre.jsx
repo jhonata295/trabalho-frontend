d924691bda5d47e1e5ed491e5dff8398f0d3b27c
jhonata295 <jhonata.matos@iesb.edu.br>
2026-06-05 23:46:49 -0300

    feat: criar página sobre

---
+++ trabalho-frontend/src/pages/Sobre.jsx
@@ -0,0 +1,23 @@
+function Sobre() {
+  return (
+    <div>
+      <h1>Sobre o Projeto</h1>
+
+      <p>
+        O FutManager é um sistema desenvolvido em React para gerenciamento de
+        times de futebol.
+      </p>
+
+      <h2>Tecnologias Utilizadas</h2>
+
+      <ul>
+        <li>React</li>
+        <li>React Router</li>
+        <li>Context API</li>
+        <li>Axios</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sobre;
